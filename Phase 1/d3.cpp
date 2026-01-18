4838c65345990dcacafb45e4a8d12f919a40e9b8
abhijai001 <abhijaiadditional@gmail.com>
2026-01-19 02:16:04 +0530

    DAY 3:OPERATORS AND IF ELSE

--- Phase 1/d3.cpp
+++ Phase 1/d3.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    //positive , negative or zero
+    int a;
+    cin>>a;
+    if(a>0){
+        cout<<"positive";
+    }else if(a==0){
+        cout<<"zero";
+    }else{
+        cout<<"negative";
+    }
 
     return 0;
 }
