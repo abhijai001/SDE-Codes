42082c4e084ab619bb2945d64659ae54b14ea3e6
abhijai001 <abhijaiadditional@gmail.com>
2026-01-19 18:03:48 +0530

    DAY 05: ARRAY

--- Phase 1/DAY 05/maxmin.cpp
+++ Phase 1/DAY 05/maxmin.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int arr[10],max =0,min=arr[0];
+    for(int i =0;i<10;i++){
+        cout<<"Enter the elements";
+        cin>>arr[i];
+    }
+    for(int i=0;i<10;i++){
+        if(arr[i]>max){
+            max=arr[i];   
+        }
+    }
+    for(int i =0;i<10;i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    cout<<"The maximum value is :- "<<max<<endl<<"The minimum value is:- "<<min<<endl;
     return 0;
 }
