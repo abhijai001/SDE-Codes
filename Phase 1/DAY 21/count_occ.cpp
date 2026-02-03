fbe0b71255a4a10c34dc32a7f112dc71cf5ef6a7
abhijai001 <abhijaiadditional@gmail.com>
2026-02-03 14:40:01 +0530

    DAY 21: BINARY SEARCH

--- Phase 1/DAY 21/count_occ.cpp
+++ Phase 1/DAY 21/count_occ.cpp
@@ -1,54 +1,64 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int firstOcc(const vector<int>& a, int target){
-    int low = 0, high = (int)a.size() - 1, ans = -1;
-    while(low <= high){
-        int mid = low + (high - low)/2;
-
-        if(a[mid] == target){
-            ans = mid;        // store
-            high = mid - 1;   // left side
-        } else if(a[mid] > target){
-            high = mid - 1;
-        } else {
-            low = mid + 1;
+int firstocc(const vector<int>& a, int target){
+    int low=0,high =(int)a.size()-1;
+    
+    int ans =-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(a[mid]==target){
+            ans=mid;
+            high=mid-1;
         }
+        else if(a[mid]<target){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+
     }
     return ans;
 }
-
-int lastOcc(const vector<int>& a, int target){
-    int low = 0, high = (int)a.size() - 1, ans = -1;
-    while(low <= high){
-        int mid = low + (high - low)/2;
-
-        if(a[mid] == target){
-            ans = mid;        // store
-            low = mid + 1;    // right side
-        } else if(a[mid] > target){
-            high = mid - 1;
-        } else {
-            low = mid + 1;
+int lastocc(const vector<int>& a, int target){
+    int low=0,high =(int)a.size()-1;
+    
+    int ans =-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(a[mid]==target){
+            ans=mid;
+            low=mid+1;
         }
+        else if(a[mid]<target){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+
     }
     return ans;
 }
-
-int countOcc(const vector<int>& a, int target){
-    int f = firstOcc(a, target);
-    if(f == -1) return 0;          // not found
-
-    int l = lastOcc(a, target);
-    return l - f + 1;              // formula
-}
-
 int main(){
-    int n; cin >> n;
-    vector<int> a(n);
-    for(int i=0;i<n;i++) cin >> a[i];
-    int target; cin >> target;
+    int n;
+    cin>>n;
 
-    cout << countOcc(a, target);
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    int target;
+    cin>>target;
+
+    int first = firstocc(a,target);
+    if(first == -1){
+        cout<<"COUNT : 0\n";
+        return 0;
+    }
+    int last =lastocc(a,target);
+    cout<<"COUNT: "<<(last - first + 1)<<endl;
     return 0;
+    
 }
--- Phase 1/DAY 21/first_occ.cpp
+++ Phase 1/DAY 21/first_occ.cpp
@@ -1,39 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-int firocc(const vector<int>& a,int target ){
-    int low=0;
-    int high=a.size()-1;
-    
+int firocc(const vector<int>& v,int target){
+    int low  =0,high=(int)v.size()-1;
     int ans=-1;
     while(low<=high){
-        int mid=low+(high-low)/2;
-        if(a[mid]==target){
+        int mid = low+(high-low)/2;
+        if(v[mid]==target){
             ans=mid;
-            high = mid -1;
-        }else if(a[mid]>target){
-            high = mid-1;
-        }else if(a[mid]<target){
-            low= mid+1;
+            high =mid-1;
         }
+        else if(v[mid]<target){
+            low=mid+1;
+        }else{
+            high =mid-1;
+        }
+
     }
 
     return ans;
 }
 int main(){
     int n;
-    cout<<"Enter the size of element: ";
+    cout<<"Enter the size: ";
     cin>>n;
-    vector<int> a(n);
-    cout<<"Enter the elements: ";
+    vector<int>v(n);
+    cout<<"Enter sorted elements: ";
     for(int i=0;i<n;i++){
-        cin>>a[i];
+        cin>>v[i];
     }
-
-    cout<<"Enter the target: ";
     int target;
+    cout<<"Enter the target: ";
     cin>>target;
-
-    cout<<firocc(a,target);
-
+    cout<<"First occurance is: "<<firocc(v,target);
+    
     return 0;
 }
