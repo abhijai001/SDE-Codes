2ea6601106995155dae2131dd7ff98fb1dd239d7
abhijai001 <abhijaiadditional@gmail.com>
2026-02-03 15:02:30 +0530

    DAY 21:BINARY SEARCH PROBLEMS

--- Phase 1/DAY 21/insert_pos.cpp
+++ Phase 1/DAY 21/insert_pos.cpp
@@ -1,29 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int insertPos(const vector<int>& a, int target){
-    int low = 0, high = (int)a.size() - 1;
-    int ans = (int)a.size();   // default: end
-
-    while(low <= high){
+int insert(const vector<int>& a, int target){
+    int low =0;
+    int high = (int)a.size(); // -1 nhi hai kyuki index ans wala last me bhi ho sakta hai alst ke baad bhi 
+    while(low<high){ // yaha <= is liye nhi hai kyuki jb hum low == high jayenge wahi answer hoga
         int mid = low + (high - low)/2;
-
-        if(a[mid] >= target){
-            ans = mid;         // possible position
-            high = mid - 1;    // left me aur chhota index dhoondo
-        } else {
-            low = mid + 1;     // right side
+        if(a[mid]>=target){
+            high =mid;
+        }
+        else{
+            low = mid+1;
         }
     }
-    return ans;
+    return low; // kyuki loop ktm ho rha hai low == high pe , low wo index hai jaha a[mid]>= target se;
+
 }
-
 int main(){
-    int n; cin >> n;
-    vector<int> a(n);
-    for(int i=0;i<n;i++) cin >> a[i];
-    int target; cin >> target;
+    int n;
+    cout<<"Enter the size:";
+    cin>>n;
 
-    cout << insertPos(a, target);
+    vector<int> a(n);
+    cout<<"Enter the elements: ";
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    cout<<"Enter the target: ";
+    int target;
+    cin>>target;
+
+    cout<<"The Position: "<<insert(a,target);
+
     return 0;
 }
