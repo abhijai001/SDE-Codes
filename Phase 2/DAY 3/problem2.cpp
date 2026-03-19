1339d29499df8009961072d8906835fc830fddb9
abhijai001 <abhijaiadditional@gmail.com>
2026-03-19 13:34:54 +0530

    problem 2

---
+++ Phase 2/DAY 3/problem2.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int rev(int n){
+    int rev = 0;
+    while(n!=0){
+        int digit = n%10;
+        rev = rev*10+digit;
+        n=n/10;
+    }
+    return rev;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<rev(n);
+    
+    return 0;
+}
