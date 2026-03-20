858a8133a5a858f37bbbae0e3a5137817eb48a1e
abhijai001 <abhijaiadditional@gmail.com>
2026-03-21 00:11:19 +0530

    Pair sum problem

---
+++ Phase 2/DAY 4/pairsum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> pairsum(vector<int> nums, int target){
+    target = 6;
+    vector<int>ans;
+    for(int i=0;i<nums.size();i++){
+        for(int j=i;j<nums.size();j++){
+            if(nums[i]+nums[j]==target){
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+            }
+        }
+    } 
+    return ans;
+}
+int main(){
+    vector<int> nums={1,2,3,5};
+    int target;
+    vector<int> ans= pairsum(nums,target);
+    cout<<ans[0]<<", "<<ans[1]<<endl;
+    
+    return 0;
+}
