326953efb072ceee02e70f2bd81a05d2466002e5
abhijai001 <abhijaiadditional@gmail.com>
2026-03-29 00:34:56 +0530

    Book allocation for all

---
+++ Phase 2/DAY 7/bookall.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+bool isvalid(vector<int> &arr,int n,int m,int mid){
+    int stu =1,pages=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]>mid){
+            return false;
+        }
+        if(pages+arr[i]<=mid){
+            pages+=arr[i];
+        }
+        else{
+            stu++;
+            pages =arr[i];
+        }
+    }
+    return stu  >m?false:true;
+}
+int allocatebooks(vector<int>&arr,int n,int m){
+    int sum =0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    int ans=-1;
+    int st=0;
+    int end=n-1;
+    while(st<=end){
+        int mid=st+(end-st)/2;
+        if(isvalid(arr,n,m,mid)){
+            ans=mid;
+            end=mid-1;
+        }else{
+            st=mid-1;
+        }
+    }
+    return ans;
+}
+int main(){
+    vector<int> arr={2,1,3,4};
+    int n=4,m=2;
+
+    cout<<allocatebooks(arr,n,m)<<endl;
+    return 0;
+}
